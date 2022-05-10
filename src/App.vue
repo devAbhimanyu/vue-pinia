94cf13d35f94d3382d66236238e27e7b754f4371
devabhimanyu <dev.chauhan.singh.abhimanyu@gmail.com>
2022-05-10 22:07:45 +0530

    :tada: initial project setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <router-link to="/">Notes </router-link> |
+    <router-link to="/stats">Stats </router-link>
+    <router-view></router-view>
+  </main>
+</template>
+
+<style></style>
